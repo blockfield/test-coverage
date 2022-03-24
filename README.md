00cbfc0e29b0a97d926585921ed15e394f8f4873
Drygin <DryginAlexander@gmail.com>
2022-03-24 16:43:59 +0300

    wip3

--- README.md
+++ README.md
@@ -4,6 +4,5 @@
 
 [ci-image]: https://github.com/blockfield/test-coverage/workflows/test/badge.svg
 [ci-url]: https://github.com/blockfield/test-coverage/actions?workflow=test
-
 [coveralls-image]: https://coveralls.io/repos/github/blockfield/test-coverage/badge.svg?branch=main
 [coveralls-url]: https://coveralls.io/github/blockfield/test-coverage?branch=main
--- contracts/FundsReceiver.sol
+++ contracts/FundsReceiver.sol
@@ -11,16 +11,10 @@ contract FundsReceiver is Ownable {
     function claimTokens(address token, uint256 amount) external onlyOwner {
         address payable ownerPayable = payable(owner());
         if (token == address(0)) {
-            if (amount == 0) {
-                amount = address(this).balance;
-            }
             ownerPayable.transfer(amount);
             return;
         }
 
-        if (amount == 0) {
-            amount = IERC20(token).balanceOf(address(this));
-        }
         IERC20(token).transfer(ownerPayable, amount);
 
         emit ClaimedTokens(token, ownerPayable, amount);
